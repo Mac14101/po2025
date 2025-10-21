846eef909c645826eff2089a55b9886fd766ca66
Maciej Babral <maciejbabral@student.agh.edu.pl>
2025-10-21 19:46:15 +0200

    Wykonanie zadania Array-2 - shiftLeft

--- Lab3/src/CodingBat.java
+++ Lab3/src/CodingBat.java
@@ -14,4 +14,17 @@ public class CodingBat {
             return "not "+str;
         }
     }
+    // Array2 > shiftLeft
+    public int[] shiftLeft(int[] nums) {
+        if(nums.length==0){
+            return nums;
+        }
+        int buff=nums[0];
+        for(int i=0;i<nums.length-1;i++){
+            nums[i]=nums[i+1];
+        }
+        nums[nums.length-1]=buff;
+        return nums;
+    }
+
 }
--- Lab3/src/CodingBatTest.java
+++ Lab3/src/CodingBatTest.java
@@ -19,4 +19,14 @@ public class CodingBatTest {
         assertEquals("not is not",new CodingBat().notString("is not"));
         assertEquals("not no",new CodingBat().notString("no"));
     }
+    @org.junit.Test
+    public void shiftLeft() {
+        assertArrayEquals(new int[]{2, 5, 3, 6},new CodingBat().shiftLeft(new int[]{6, 2, 5, 3}));
+        assertArrayEquals(new int[]{2, 1},new CodingBat().shiftLeft(new int[]{1, 2}));
+        assertArrayEquals(new int[]{1},new CodingBat().shiftLeft(new int[]{1}));
+        assertArrayEquals(new int[]{},new CodingBat().shiftLeft(new int[]{}));
+        assertArrayEquals(new int[]{1, 2, 2, 4, 1},new CodingBat().shiftLeft(new int[]{1, 1, 2, 2, 4}));
+        assertArrayEquals(new int[]{1, 1, 1},new CodingBat().shiftLeft(new int[]{1, 1, 1}));
+        assertArrayEquals(new int[]{2, 3, 1},new CodingBat().shiftLeft(new int[]{1, 2, 3}));
+    }
 }
