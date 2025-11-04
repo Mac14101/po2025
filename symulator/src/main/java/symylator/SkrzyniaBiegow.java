7d4173d6f34a2256e784ccec44301e37dd380619
Maciej Babral <maciejbabral@student.agh.edu.pl>
2025-11-04 19:41:16 +0100

    Dodanie dodatkowych konstruktorów w klasie 'SkrzyniaBiegow' oraz atrybutu sprzeglo

--- symulator/src/main/java/symylator/SkrzyniaBiegow.java
+++ symulator/src/main/java/symylator/SkrzyniaBiegow.java
@@ -4,12 +4,29 @@ public class SkrzyniaBiegow extends Komponent {
     private int aktualnyBieg;
     private int iloscBiegow;
     private double aktualnePrzelozenie;
+    Sprzeglo sprzeglo;
 
-    public SkrzyniaBiegow(int iloscBiegow, double waga, double cena) {
+    public SkrzyniaBiegow(int aktualnyBieg, int iloscBiegow, double aktualnePrzelozenie, Sprzeglo sprzeglo, double waga, double cena) {
+        super("Skrzynia biegów", waga, cena);
+        this.aktualnyBieg = aktualnyBieg;
+        this.iloscBiegow = iloscBiegow;
+        this.aktualnePrzelozenie = aktualnePrzelozenie;
+        this.sprzeglo = sprzeglo;
+    }
+
+    public SkrzyniaBiegow(int iloscBiegow, Sprzeglo sprzeglo, double waga, double cena) {
         super("Skrzynia biegów", waga, cena);
         this.iloscBiegow = iloscBiegow;
         this.aktualnyBieg = 0;
         this.aktualnePrzelozenie = 0;
+        this.sprzeglo = sprzeglo;
+    }
+
+    public SkrzyniaBiegow() {
+        super("Skrzynia biegów", 45d, 16000d);
+        this.iloscBiegow = 5;
+        this.aktualnyBieg = 0;
+        this.sprzeglo = new Sprzeglo();
     }
 
     void zwiekszBieg() {
