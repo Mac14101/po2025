78929d6106069970ae68508371d1a327f727025e
Maciej Babral <maciejbabral@student.agh.edu.pl>
2025-11-04 19:35:51 +0100

    Dodanie dodatkowych konstruktorów w klasie 'Sprzeglo'

--- symulator/src/main/java/symylator/Sprzeglo.java
+++ symulator/src/main/java/symylator/Sprzeglo.java
@@ -4,8 +4,18 @@ public class Sprzeglo extends Komponent {
     // Wartość 'false' oznacza sprzęgło wciścięte, 'true' oznacza sprzęgło zwolnione
     private boolean stanSprzegla;
 
+    public Sprzeglo(boolean stanSprzegla, double waga, double cena) {
+        super("Sprzęgło", waga, cena);
+        this.stanSprzegla = stanSprzegla;
+    }
+
     public Sprzeglo(double waga, double cena) {
         super("Sprzęgło", waga, cena);
+        this.stanSprzegla = true;
+    }
+
+    public Sprzeglo() {
+        super("Sprzęgło", 6d, 1800d);
         this.stanSprzegla = true;
     }
 
