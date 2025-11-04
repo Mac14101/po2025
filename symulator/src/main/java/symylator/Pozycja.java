8d9eab2e0a2fbdbd0cf5d1d1839c3ba4cf6321ae
Maciej Babral <maciejbabral@student.agh.edu.pl>
2025-11-04 19:24:21 +0100

    Implementacja klasy 'Pozycja'

---
+++ symulator/src/main/java/symylator/Pozycja.java
@@ -0,0 +1,16 @@
+package symylator;
+
+public class Pozycja {
+    private double x;
+    private double y;
+
+    public Pozycja(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Pozycja() {
+        this.x = 0d;
+        this.y = 0d;
+    }
+}
