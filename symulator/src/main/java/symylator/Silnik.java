cf52599aafd0c9e92f531f14c77829958cd966f0
Maciej Babral <maciejbabral@student.agh.edu.pl>
2025-11-04 19:32:46 +0100

    Dodanie dodatkowych konstruktorów w klasie 'Silnik'

--- symulator/src/main/java/symylator/Silnik.java
+++ symulator/src/main/java/symylator/Silnik.java
@@ -4,9 +4,21 @@ public class Silnik extends Komponent {
     private int maxObroty;
     private int obroty;
 
+    public Silnik(int maxObroty, int obroty, double waga, double cena) {
+        super("Silnik", waga, cena);
+        this.maxObroty = maxObroty;
+        this.obroty = obroty;
+    }
+
     public Silnik(int maxObroty, double waga, double cena) {
         super("Silnik", waga, cena);
         this.maxObroty = maxObroty;
+        this.obroty = 0;
+    }
+
+    public Silnik() {
+        super("Silnik", 100d, 15000d);
+        this.maxObroty = 8000;
         this.obroty = 0;
     }
 
