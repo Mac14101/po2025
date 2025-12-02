4a553b748d25eb50827718ac7c17e1080f7aff0b
Maciej Babral <mbabral105@gmail.com>
2025-12-02 17:31:41 +0100

    Dodanie obsługi zdarzeń przycisków z sekcji 'Skrzynia biegów'

--- gui/src/main/java/gui/gui/SamochoGUIControler.java
+++ gui/src/main/java/gui/gui/SamochoGUIControler.java
@@ -5,22 +5,39 @@ import javafx.scene.control.Button;
 
 public class SamochoGUIControler {
     @FXML
-    public Button startButton;
+    private Button startButton;
     @FXML
-    public Button stopButton;
+    private Button stopButton;
     @FXML
-    public Button buttonButton;
+    private Button buttonButton;
 
     @FXML
-    private void  onStartButton(){
+    private Button upshiftGear;
+    @FXML
+    private Button downshiftGear;
+
+    @FXML
+    private void onStartButton() {
         System.out.println("StartButton click.");
     }
+
     @FXML
-    private void  onStopButton(){
+    private void onStopButton() {
         System.out.println("StopButton click.");
     }
+
     @FXML
-    private void  onButtonButton(){
+    private void onButtonButton() {
         System.out.println("ButtonButton click.");
+    }
+
+    @FXML
+    private void onUpshiftGear() {
+        System.out.println("UpshiftGearButton");
+    }
+
+    @FXML
+    private void onDownshiftGear() {
+        System.out.println("DownshiftGearButton");
     }
 }
