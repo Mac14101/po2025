7b47b07c838533842808c43f065601a99507bc65
Mac1410 <mbabral105@gmail.com>
2025-12-16 21:23:31 +0100

    style: dodanie stylów do elementów głównego okna aplikacji oraz poprawa ustawienia elementów bocznego menu
    Dodano plik style.css, który zawiera klasy css użyte w pliku fxml z widokiem głównego okna aplikacji. Poprawiono ustawienie elementów menu bocznego, aby było brdziej przejżyste.

--- gui/src/main/java/gui/gui/SamochodGUIApplication.java
+++ gui/src/main/java/gui/gui/SamochodGUIApplication.java
@@ -11,7 +11,7 @@ public class SamochodGUIApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SamochodGUIApplication.class.getResource("map.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
