b90d0552d6dd5ff501ce4272134dda0e2b2b705c
Michal Firek <mfirek@student.agh.edu.pl>
2025-12-27 11:52:57 +0100

    dashboard

--- edziennikUI/src/main/java/com/example/edziennikui/shared/DashboardController.java
+++ edziennikUI/src/main/java/com/example/edziennikui/shared/DashboardController.java
@@ -2,8 +2,6 @@ package com.example.edziennikui.shared;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class DashboardController {
 
