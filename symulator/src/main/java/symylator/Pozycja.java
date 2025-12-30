cfeaff7f92d77aa35b0074db649f9aa0fd0ff0ae
Maciej Babral <maciejbabral@student.agh.edu.pl>
2025-12-30 20:57:05 +0100

    fix: Poprawiono dokładność funkcji 'przemieść()'
    Poprawiono dokładność funkcji 'przemieść()' dla ostatniego przeskoku.

--- symulator/src/main/java/symylator/Pozycja.java
+++ symulator/src/main/java/symylator/Pozycja.java
@@ -15,10 +15,17 @@ public class Pozycja {
     }
 
     public void przemiesc(Pozycja cel, double predkosc, double dt) {
-        double distance = Math.sqrt(Math.pow(this.x - cel.getX(), 2) + Math.pow(this.y - cel.getY(), 2));
-        double deltaDistance = predkosc * dt;
-        this.x = this.x + deltaDistance * (cel.getX() - this.x) / distance;
-        this.y = this.y + deltaDistance * (cel.getY() - this.y) / distance;
+        double distance = Math.sqrt(Math.pow(cel.getX() - this.x, 2) + Math.pow(cel.getY() - this.y, 2));
+        double dx = predkosc * dt * (cel.getX() - this.x) / distance;
+        double dy = predkosc * dt * (cel.getY() - this.y) / distance;
+        if (Math.abs(dx) > distance) {
+            dx = cel.getX() - this.x;
+        }
+        if (Math.abs(dy) > distance) {
+            dy = cel.getY() - this.y;
+        }
+        this.x += dx;
+        this.y += dy;
     }
 
     public double getX() {
--- symulator/src/test/java/symylator/PozycjaTest.java
+++ symulator/src/test/java/symylator/PozycjaTest.java
@@ -1,6 +1,6 @@
 package symylator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PozycjaTest {
 
@@ -10,29 +10,33 @@ public class PozycjaTest {
         double startY = 0d;
         double predkosc = 1;
         double dt = 0.01;
-        Pozycja pozycja = new Pozycja(startX, startY);
-        double destinationX = 1d;
-        double destinationY = 1d;
-        pozycja.przemiesc(new Pozycja(destinationX, destinationY), predkosc, dt);
-        assertEquals(0.01, pozycja.getX(), 0.005);
-        assertEquals(0.01, pozycja.getY(), 0.005);
-        pozycja = new Pozycja(startX, startY);
-        destinationX = -1d;
-        destinationY = -1d;
-        pozycja.przemiesc(new Pozycja(destinationX, destinationY), predkosc, dt);
-        assertEquals(-0.01, pozycja.getX(), 0.005);
-        assertEquals(-0.01, pozycja.getY(), 0.005);
-        pozycja = new Pozycja(startX, startY);
-        destinationX = 1d;
-        destinationY = -1d;
-        pozycja.przemiesc(new Pozycja(destinationX, destinationY), predkosc, dt);
-        assertEquals(0.01, pozycja.getX(), 0.005);
-        assertEquals(-0.01, pozycja.getY(), 0.005);
-        pozycja = new Pozycja(startX, startY);
-        destinationX = -1d;
-        destinationY = 1d;
-        pozycja.przemiesc(new Pozycja(destinationX, destinationY), predkosc, dt);
-        assertEquals(-0.01, pozycja.getX(), 0.005);
-        assertEquals(0.01, pozycja.getY(), 0.005);
+        Pozycja start = new Pozycja(startX, startY);
+        Pozycja end = new Pozycja(1d, 1d);
+        while (start.getX() != end.getX() && start.getY() != end.getY()) {
+            start.przemiesc(end, predkosc, dt);
+        }
+        assertEquals(start.getX(), end.getX(), 0.005);
+        assertEquals(start.getY(), end.getY(), 0.005);
+        start = new Pozycja(startX, startY);
+        end = new Pozycja(-1d, -1d);
+        while (start.getX() != end.getX() && start.getY() != end.getY()) {
+            start.przemiesc(end, predkosc, dt);
+        }
+        assertEquals(start.getX(), end.getX(), 0.005);
+        assertEquals(start.getY(), end.getY(), 0.005);
+        start = new Pozycja(startX, startY);
+        end = new Pozycja(-1d, 1d);
+        while (start.getX() != end.getX() && start.getY() != end.getY()) {
+            start.przemiesc(end, predkosc, dt);
+        }
+        assertEquals(start.getX(), end.getX(), 0.005);
+        assertEquals(start.getY(), end.getY(), 0.005);
+        start = new Pozycja(startX, startY);
+        end = new Pozycja(1d, -1d);
+        while (start.getX() != end.getX() && start.getY() != end.getY()) {
+            start.przemiesc(end, predkosc, dt);
+        }
+        assertEquals(start.getX(), end.getX(), 0.005);
+        assertEquals(start.getY(), end.getY(), 0.005);
     }
 }
