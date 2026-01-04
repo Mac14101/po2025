ce3d6db349c9ce00242341208a852fb0401dde2d
Mac1410 <maciejbabral@student.agh.edu.pl>
2026-01-04 21:42:10 +0100

    feat: Implementacja klasu Student
    Dodano klasę student, która jest odwzorowaniem rekordu z tabeli 'students'. Obiekt używa klas 'User' i 'SchoolGroup', aby odwzorować relację z bazy danych.

---
+++ server/src/main/java/server/database/entities/Student.java
@@ -0,0 +1,32 @@
+package server.database.entities;
+
+public class Student {
+    User user;
+    SchoolGroup schoolGroup;
+
+    public Student(User user, SchoolGroup schoolGroup) {
+        this.user = user;
+        this.schoolGroup = schoolGroup;
+    }
+
+    public Student() {
+        this.user = null;
+        this.schoolGroup = null;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public SchoolGroup getSchoolGroup() {
+        return schoolGroup;
+    }
+
+    public void setSchoolGroup(SchoolGroup schoolGroup) {
+        this.schoolGroup = schoolGroup;
+    }
+}
