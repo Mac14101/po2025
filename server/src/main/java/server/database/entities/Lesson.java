dc25d5fabf4fdc9a90f5bd0c48ef225c97e88b3c
Mac1410 <maciejbabral@student.agh.edu.pl>
2026-01-04 21:36:31 +0100

    feat: Implementacja klasy Lekcji
    Dodano klasę 'Lesson' reprezentującą rekord z tabeli 'lessons' oraz metody pozwalające na zmianę i odczytywanie zawartosci pól

---
+++ server/src/main/java/server/database/entities/Lesson.java
@@ -0,0 +1,44 @@
+package server.database.entities;
+
+public class Lesson {
+    private Integer id;
+    private String topic;
+    private String date;
+
+    public Lesson(Integer id, String topic, String date) {
+        this.id = id;
+        this.topic = topic;
+        this.date = date;
+    }
+
+    public Lesson() {
+        this.id = null;
+        this.topic = null;
+        this.date = null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
