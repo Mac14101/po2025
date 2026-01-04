56a86112975270c8d34cf782e4157d3d80a87289
Mac1410 <maciejbabral@student.agh.edu.pl>
2026-01-04 22:04:03 +0100

    feat: Implementacja klasy Attendance
    Dodano klasę Attendance, która reprezentuje rekord w tabeli 'attendance'. Dodano także metody pozwalające na zmianę i odczytywanie zawartosci pól.

---
+++ server/src/main/java/server/database/entities/Attendance.java
@@ -0,0 +1,44 @@
+package server.database.entities;
+
+public class Attendance {
+    private User student;
+    private Lesson lesson;
+    private String status;
+
+    public Attendance(User student, Lesson lesson, String status) {
+        this.student = student;
+        this.lesson = lesson;
+        this.status = status;
+    }
+
+    public Attendance() {
+        this.student = null;
+        this.lesson = null;
+        this.status = null;
+    }
+
+    public User getStudent() {
+        return student;
+    }
+
+    public void setStudent(User student) {
+        this.student = student;
+    }
+
+    public Lesson getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
