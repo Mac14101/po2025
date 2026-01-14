85a2859cd9f4db94ed81b77cd9cc6278b0f273cd
Mac1410 <maciejbabral@student.agh.edu.pl>
2026-01-14 22:46:36 +0100

    fix: Dodano dodatkowy warunek odwierzenia tokenu
    Dodano warunek, który pozwala na odwierzenie tokenu tylko gdy użytkownik posiada nawiązaną sesje.

--- jexp/src/main/java/jexp/defaultHandlers/SessionRefreshHandler.java
+++ jexp/src/main/java/jexp/defaultHandlers/SessionRefreshHandler.java
@@ -5,9 +5,12 @@ import jexp.*;
 public class SessionRefreshHandler implements Handler {
     @Override
     public void handle(Request request, Response response, Next next) throws JExpError {
+        if (!request.sessionEstabilished()) {
+            response.status(401);
+        }
         String newToken = request.refreshSession();
         response.type("application/json");
         response.status(200);
-        response.send("{\"token\":\"" + newToken + "\"}");
+        response.send(newToken);
     }
 }
