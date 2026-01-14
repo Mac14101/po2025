091a70a8a6a7cbbedaa1defd0f8c3bf69c4250c5
Mac1410 <maciejbabral@student.agh.edu.pl>
2026-01-14 23:07:00 +0100

    feat: Implementacja zarządzania sesją po stronie klienta w klasie 'Client'

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,22 +1,65 @@
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
 
-public class Client {
+public class Client extends Thread {
     private final HttpClient client;
+    private String token;
+    private LocalDateTime tokenTime;
 
     public Client() {
         this.client = HttpClient.newHttpClient();
     }
 
     public HttpRequest.Builder request(String url) {
-        //TODO
-        return null;
+        return HttpRequest.newBuilder(URI.create(url));
     }
 
-    // Wybrać sposób sprawdzenia statusu odpowiedzi
-    public HttpResponse<String> fetch(HttpRequest.Builder request) {
-        //TODO
-        return null;
+    public HttpResponse<String> fetch(HttpRequest.Builder request) throws Exception {
+        if (token != null) {
+            request.headers("Authorization", "Basic" + token);
+        }
+        HttpResponse<String> response = this.client.send(request.build(), HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() >= 400 && response.statusCode() < 500) {
+            throw new Exception("Something went wrong");
+        }
+        return response;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+        this.tokenTime = LocalDateTime.now();
+    }
+
+    private void refreshToken() {
+        try {
+            HttpRequest.Builder request =
+                    this.request("");
+            HttpResponse<String> response = this.fetch(request);
+            String newToken = response.body();
+            this.setToken(newToken);
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    public void run() {
+        while (true) {
+            if (this.token != null) {
+                if (this.tokenTime.plusMinutes(10).isBefore(LocalDateTime.now())) {
+                    this.token = null;
+                    this.tokenTime = null;
+                } else {
+                    this.refreshToken();
+                    try {
+                        Thread.sleep(5 * 60 * 1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
     }
 }
