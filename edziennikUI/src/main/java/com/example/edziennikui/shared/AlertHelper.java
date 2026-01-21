477e0e88abe1d3749b5b2512e46be5dc50525bda
Michal Firek <mfirek@student.agh.edu.pl>
2026-01-21 23:25:45 +0100

    Nowy alert

--- edziennikUI/src/main/java/com/example/edziennikui/shared/AlertHelper.java
+++ edziennikUI/src/main/java/com/example/edziennikui/shared/AlertHelper.java
@@ -1,6 +1,9 @@
 package com.example.edziennikui.shared;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class AlertHelper {
 
@@ -22,5 +25,15 @@ public class AlertHelper {
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    public static boolean showConfirmation(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
     }
 }
